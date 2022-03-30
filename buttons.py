005e652e59d8bbede3d33db300f5a8dc8e1f83c9
AC-Oliveira <allancardoso21@gmail.com>
2022-03-30 19:36:08 -0300

    feat: game logic implemented with bugs

--- buttons.py
+++ buttons.py
@@ -1,32 +1,70 @@
 from tkinter import * # type: ignore
 from PIL import ImageTk, Image
 
-class Buttons:
-  def __init__(self, window, bombs, side_numbers_count) -> None:
-    self.window = window
-    self.bombs = bombs
-    self.side_numbers_count = side_numbers_count
 
+class Buttons:
+  def __init__(self, window, bombs, side_numbers_count, empty_squares) -> None:
+    self.window = window
+    self.bombs_coordinates = bombs
+    self.side_numbers_count = side_numbers_count
+    self.empty_squares = empty_squares
+    self.side_empties = []
+    self.side_numbers = []
 
 
   def create_button(self, number) -> None:
     def handle_click(number) -> None:
       myButton.destroy()
-      if number in self.bombs:
+      if number in self.bombs_coordinates:
+        print('Perdeu!')
         img = ImageTk.PhotoImage(Image.open('icons8-naval-mine-25.png')) # type: ignore
         myLabel = Label(self.window, image=img)
         myLabel.image = img # type: ignore
         myLabel.grid(row=number//9 , column=number%9)
+        myLabel.columnconfigure(number, weight=1)
+        myLabel.rowconfigure(number, weight=1)
       elif number in self.side_numbers_count:
-        myLabel = Label(self.window, text=f'{self.side_numbers_count[number]}', borderwidth=4, relief="sunken")
-        myLabel.grid(row=number//9 , column=number%9, padx=30,pady=30)
-      else:
-        myLabel = Label(self.window, text='  ', borderwidth=4, relief="sunken")
+        myLabel = Label(self.window, text=f'{self.side_numbers_count[number]} ', borderwidth=4, relief="sunken")
         myLabel.grid(row=number//9 , column=number%9)
-    
-    border = Frame(self.window, highlightbackground='blue', highlightthickness=2, padx=20, pady=20)
+        myLabel.columnconfigure(number, weight=1)
+        myLabel.rowconfigure(number, weight=1)
+      else:
+        self.side_empty_list(number)
+        for item in self.side_empties:
+          Buttons.side_empty_list(self, item)
+          myLabel = Label(self.window, text='  ', borderwidth=4, relief="sunken")
+          myLabel.grid(row=item//9 , column=item%9)
+          myLabel.columnconfigure(item, weight=1)
+          myLabel.rowconfigure(item, weight=1)
+        for item in self.side_numbers:
+          myLabel = Label(self.window, text=f'{self.side_numbers_count[item]}', borderwidth=4, relief="sunken")
+          myLabel.grid(row=item//9 , column=item%9)
+          myLabel.columnconfigure(item, weight=1)
+          myLabel.rowconfigure(item, weight=1)
 
+
+    # border = Frame(self.window, highlightbackground='blue', highlightthickness=2, padx=20, pady=20)
     myButton = Button(
-      border, text=" ", command=lambda: handle_click(number)
+      self.window, text=' ', command=lambda: handle_click(number)
     )
-    border.grid(row=number//9 , column=number%9)
+    myButton.grid(row=number//9 , column=number%9)
+
+
+  def side_empty_list(self, number: int) -> None:
+    number_to_coordinate = lambda _number: [_number//9, _number%9]
+    coordinate_to_number = lambda _coordinate: _coordinate[0]*9 + _coordinate[1]
+    number_coordinate = number_to_coordinate(number)
+    i = -1
+    while i <= 1:
+      if 0 <= number_coordinate[0] + i <= 8:
+        j = -1
+        while j <= 1:
+          curent_number = coordinate_to_number([number_coordinate[0] + i, number_coordinate[1] + j])
+          if 0 <= number_coordinate[1] + j <= 8 and curent_number not in self.bombs_coordinates  and (
+            curent_number not in self.side_empties and curent_number not in self.side_numbers_count.keys()
+            ):
+            self.side_empties.append(curent_number)
+          elif curent_number in self.side_numbers_count.keys():
+            self.side_numbers.append(curent_number)
+          j += 1
+      i += 1
--- minesweeper.py
+++ minesweeper.py
@@ -1,65 +1,86 @@
 from tkinter import * # type: ignore
 from random import randint
 
+from numpy import empty
+
 from buttons import Buttons
 
 class App:
   def __init__(self) -> None:
     # Define window and game properties.
-    self.window = Tk()
-    self.bombs = []
-    self.side_numbers_count = {}
-
-  def side_numbers(self):
-    def numbers_to_cordinates(number):
-      NINE = 9
-      return [number // NINE, number % NINE]
-
-    def coordinates_to_numbers(cordinates: list) -> list:
-      numbers_list = list(map(lambda cordinate: cordinate[0] * 9 + cordinate[1],cordinates))
-      numbers_list.sort()
-      return numbers_list
+    self.window: Tk  = Tk()
+    self.bombs_coordinates: list = []
+    self.bombs_side_numbers: dict = {}
+    self.empty_squares: list = []
 
 
-    bombs_coordinates = []
-    side_bombs_coordinates = []
-    for number in self.bombs:
-      bombs_coordinates.append(numbers_to_cordinates(number))
-    bombs_coordinates.sort()
+  def numbers_to_cordinates(self, numbers_list: list) -> list:
+    NINE = 9
+    coordinates_list = list(map(lambda number: [number // NINE, number % NINE], numbers_list))
+    return coordinates_list
 
-    for coordinate in bombs_coordinates:
+
+  def coordinates_to_numbers(self, cordinates_list: list) -> list:
+    NINE = 9
+    numbers_list = list(map(lambda coordinate: coordinate[0] * NINE + coordinate[1],cordinates_list))
+
+    return numbers_list
+
+
+  def side_numbers_list(self, numbers_list, type='numbers') -> list:
+    coordinates_list = App.numbers_to_cordinates(self, numbers_list)
+    side_coordinates_list = []
+
+    for coordinate in coordinates_list:
       i = -1
       while i <= 1:
         if 0 <= coordinate[0] + i <= 8:
           j = -1
           while j <= 1:
             currrent_coordinate = [coordinate[0] + i, coordinate[1]+j]
-            if 0 <= coordinate[1] + j <= 8 and currrent_coordinate not in bombs_coordinates:
-              side_bombs_coordinates.append([coordinate[0]+i, coordinate[1]+j])
+            if type == 'numbers' and 0 <= coordinate[1] + j <= 8 and currrent_coordinate not in coordinates_list:
+              side_coordinates_list.append([coordinate[0]+i, coordinate[1]+j])
+
+            elif 0 <= coordinate[1] + j <= 8 and coordinate[0] * 9 + coordinate[1] in self.empty_squares:
+              side_coordinates_list.append([coordinate[0]+i, coordinate[1]+j])
+
             j += 1
         i += 1
-    side_bombs_coordinates.sort()
-    side_numbers_list = coordinates_to_numbers(side_bombs_coordinates)
+
+    side_numbers_list = App.coordinates_to_numbers(self, side_coordinates_list)
+    
+    return side_numbers_list
 
 
+  def side_numbers_count(self, side_numbers_list):
     for number in side_numbers_list:
-      if number in self.side_numbers_count:
-        self.side_numbers_count[number] += 1
+      if number in self.bombs_side_numbers:
+        self.bombs_side_numbers[number] += 1
       else:
-        self.side_numbers_count[number] = 1
+        self.bombs_side_numbers[number] = 1
 
 
-  def random_bombs(self):
+
+  def side_empty_squares(self):
+    n = 0
+    while n <= 80:
+      if n not in self.bombs_coordinates and n not in self.bombs_side_numbers.keys():
+        self.empty_squares.append(n)
+      n += 1
+
+
+  def random_bombs(self) -> None:
     #generate bombs in random coordinates
-    while len(self.bombs) <= 10:
+    while len(self.bombs_coordinates) <= 9:
       number = randint(0,80)
-      if number not in self.bombs:
-        self.bombs.append(number)
+      if number not in self.bombs_coordinates:
+        self.bombs_coordinates.append(number)
 
-  def grid(self):
+
+  def grid(self) -> None:
     def label_text(number):
-      if number in self.bombs: return 'B'
-      elif  number in self.side_numbers_count: return self.side_numbers_count[number]
+      if number in self.bombs_coordinates: return 'B'
+      elif  number in self.bombs_side_numbers: return self.bombs_side_numbers[number]
       else: return '  '
 
     def create_label(number) -> None:
@@ -74,21 +95,23 @@ class App:
       n += 1
 
 
-
-
   def startWindow(self) -> None:
     # frame.pack()
     App.random_bombs(self)
-    App.side_numbers(self)
+    App.side_numbers_count(self, App.side_numbers_list(self, self.bombs_coordinates))
+    App.side_empty_squares(self)
+
     # App.grid(self)
-    buttons = Buttons(self.window, self.bombs, self.side_numbers_count)
+
+    buttons = Buttons(self.window, self.bombs_coordinates, self.bombs_side_numbers, self.empty_squares)
+    # buttons.side_empty_list(10)
     n = 0
     while n < 81:
       buttons.create_button(n)
       self.window.columnconfigure(n, weight=1)
       self.window.rowconfigure(n, weight=1)
       n += 1
-    
+
     self.window.title('Campo Minado')
     # self.window.maxsize(280,320)
     # self.window.minsize(280,320)
